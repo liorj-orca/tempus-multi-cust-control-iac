0d970c5f7fbbafd1d9cdcd7c22201b43daf7897a
liorj-orca <96177663+liorj-orca@users.noreply.github.com>
2025-08-24 13:06:10 +0300

    Add files via upload

---
+++ liorjtest-file-22.tf
@@ -0,0 +1,31 @@
+module "foo_bucket" {
+  source = "git@github.com:example/repo/gcp/storage/bucket"
+
+  project_context = module.workload_context.project_context
+  name            = "foo-owners"
+  versioning      = false
+  lifecycle_rules = []
+}
+
+
+module "foo_owners_bucket_perms_4" {
+  storage_bucket = module.foo_bucket.bucket_name
+  member         = "serviceAccount:foo-service@random-project.iam.gserviceaccount.com"
+  roles = [
+    "roles/owner",
+  ]
+}
+
+module "foo_service_account_bindings_5" {
+  project_context = module.workload_context.project_context
+  member          = "serviceAccount:foo-service@random-project.iam.gserviceaccount.com"
+  roles = [
+  ]
+}
+
+module "foo_owners_bucket_perms_6" {
+  storage_bucket = module.foo_bucket.bucket_name
+  member         = "group:foo-group@random.com"
+  roles = [
+  ]
+}
